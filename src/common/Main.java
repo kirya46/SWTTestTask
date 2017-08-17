0c2b84b6b84b589720231fed7099cf38874255a3
kirya@home <kirillstoianov46@gmail.com>
2017-08-18 00:42:41 +0300

    intermediate commit

--- src/common/Main.java
+++ src/common/Main.java
@@ -7,11 +7,18 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ControlAdapter;
 import org.eclipse.swt.events.ControlEvent;
 import org.eclipse.swt.events.ControlListener;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.FileDialog;
@@ -39,11 +46,12 @@ public class Main {
 
 		// TODO add some widgets to the Shell
 		// Shell can be used as container
-//		initLable(shell);
-//		initText(shell, display);
+		// initLable(shell);
+		// initText(shell, display);
 
 		Table table = initListViewer(shell);
 		initButton(shell, table);
+		test(shell, display);
 
 		// start shell
 		shell.open();
@@ -80,27 +88,29 @@ public class Main {
 		button.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-//				System.out.println("Button pressed");
-//				TableItem item = new TableItem(table, SWT.NONE);
-//				item.setText(0, "delete");
-//				item.setText(1, System.currentTimeMillis() + "");
-				
-				 // Ask the user which files to upload
-				  FileDialog dialog =
-				      new FileDialog(Display.getCurrent().getActiveShell(), SWT.OPEN
-				          | SWT.MULTI);
-				  dialog.setText("Select the local files to upload");
-				  dialog.open();
+				// System.out.println("Button pressed");
+				// TableItem item = new TableItem(table, SWT.NONE);
+				// item.setText(0, "delete");
+				// item.setText(1, System.currentTimeMillis() + "");
 
-				  ArrayList<File> files = new ArrayList<File>();
-				  for (String fname : dialog.getFileNames())
-				    files.add(new File(dialog.getFilterPath() + File.separator + fname));
+				// Ask the user which files to upload
+				FileDialog dialog = new FileDialog(Display.getCurrent()
+						.getActiveShell(), SWT.OPEN | SWT.MULTI);
+				dialog.setText("Select the local files to upload");
+				dialog.open();
 
-//				  // TODO enable upload command only when selection is exactly one folder
-//				  List<DFSFolder> folders = filterSelection(DFSFolder.class, selection);
-//				  if (folders.size() >= 1)
-//				    uploadToDFS(folders.get(0), files);
-//				}
+				ArrayList<File> files = new ArrayList<File>();
+				for (String fname : dialog.getFileNames())
+					files.add(new File(dialog.getFilterPath() + File.separator
+							+ fname));
+
+				// // TODO enable upload command only when selection is exactly
+				// one folder
+				// List<DFSFolder> folders = filterSelection(DFSFolder.class,
+				// selection);
+				// if (folders.size() >= 1)
+				// uploadToDFS(folders.get(0), files);
+				// }
 			}
 		});
 
@@ -137,15 +147,16 @@ public class Main {
 		for (int i = 0; i < titles.length; i++) {
 			table.getColumn(i).pack();
 		}
-			
-		//Listener of all table
+
+		// Listener of all table
 		table.addListener(SWT.DefaultSelection, new Listener() {
 			public void handleEvent(Event e) {
 				String string = "";
 				TableItem[] selection = table.getSelection();
-				
-				for(TableItem tableItem :selection){
-					System.out.println("Remove table item: " + tableItem.getText(1));
+
+				for (TableItem tableItem : selection) {
+					System.out.println("Remove table item: "
+							+ tableItem.getText(1));
 					tableItem.dispose();
 				}
 			}
@@ -154,5 +165,60 @@ public class Main {
 		table.pack();
 
 		return table;
+	}
+
+	private static void test(final Shell shell, Display display) {
+		final Composite composite = new Composite(shell, SWT.NONE);
+		GridLayout layout = new GridLayout();
+		composite.setEnabled(false);
+		composite.setLayout(layout);
+		composite.setBackground(display.getSystemColor(SWT.COLOR_CYAN));
+
+		
+		final Composite mainComposite = new Composite(composite, SWT.NONE);
+	    mainComposite.setLayout(new GridLayout(1, false));
+	    mainComposite.setLayoutData(new GridData(SWT.WRAP, SWT.WRAP, true, true, 1, 1));
+		
+		final Button label = new Button(mainComposite, SWT.WRAP);
+		label.setText("Image");
+//		GridData fd = new GridData();
+//		fd.widthHint = 100;
+//		fd.heightHint =400;
+//		label.setLayoutData(fd);
+		
+		composite.pack();
+		composite.setLocation(1000, 550);
+
+		final Point[] offset = new Point[1];
+		Listener listener = new Listener() {
+			public void handleEvent(Event event) {
+				switch (event.type) {
+				case SWT.MouseDown:
+					Rectangle rect = composite.getBounds();
+					if (rect.contains(event.x, event.y)) {
+						Point pt1 = mainComposite.toDisplay(0,0);
+						Point pt2 = mainComposite.toDisplay(event.x, event.y);
+						offset[0] = new Point(pt2.x - pt1.x, pt2.y - pt1.y);
+					}
+					break;
+				case SWT.MouseMove:
+					if (offset[0] != null) {
+						Point pt = offset[0];
+						mainComposite.setLocation(event.x - pt.x, event.y - pt.y);
+					}
+					break;
+				case SWT.MouseUp:
+					offset[0] = null;
+					break;
+				}
+			}
+		};
+		
+		shell.addListener(SWT.MouseDown, listener);
+		shell.addListener(SWT.MouseUp, listener);
+		shell.addListener(SWT.MouseMove, listener);
+
+	
+
 	}
 }
